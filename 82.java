cf9ec418a94f8a29dbcb9a720282ca3b4d7b8175
mannam-maneesha <41893993+mannam-maneesha@users.noreply.github.com>
2018-08-28 10:06:54 +0530

    Create 82.java

---
+++ 82.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.ArrayList;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		float l=sc.nextFloat();
+		float b=sc.nextFloat();
+		Float area=l*b;
+		System.out.printf("%f",area);
+	}
+}
